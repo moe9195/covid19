5e9f20cbaf4917d12ff355676cc3501d7fb6151e
moe9195 <moerahmeh95@hotmail.com>
2020-03-30 12:46:38 +0300

    Updates

---
+++ precache-manifest.59e2a4d1f1d3ef1225c8d5901a79f460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f557eda9b97fe93950ec184f7bfc632",
+    "url": "/moe9195/covid19/index.html"
+  },
+  {
+    "revision": "5ecb26160159ca0c0558",
+    "url": "/moe9195/covid19/static/css/main.c38605c5.chunk.css"
+  },
+  {
+    "revision": "b1b43ee3c07768ea8479",
+    "url": "/moe9195/covid19/static/js/2.597d966a.chunk.js"
+  },
+  {
+    "revision": "dc298e637cd0d7c5b7ecb7e1e48fb565",
+    "url": "/moe9195/covid19/static/js/2.597d966a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ecb26160159ca0c0558",
+    "url": "/moe9195/covid19/static/js/main.bc1b7c15.chunk.js"
+  },
+  {
+    "revision": "04ade15ae503fcf845eb",
+    "url": "/moe9195/covid19/static/js/runtime-main.1c08e2fe.js"
+  }
+]);
